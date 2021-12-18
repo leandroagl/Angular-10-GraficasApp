8c96273e9b9233b8fc052702fe433ebeb784d366
lea <leandro.n.aguilera@gmail.com>
2021-12-18 12:22:32 -0300

    Estructura de archivos, rutas y lazyLoad

---
+++ src/app/graficas/graficas.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraficasRoutingModule } from './graficas-routing.module';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
+import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
+import { DonaComponent } from './pages/dona/dona.component';
+import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
+
+
+@NgModule({
+  declarations: [
+    BarrasComponent,
+    DonaHttpComponent,
+    GraficaBarraComponent,
+    DonaComponent,
+    BarrasDobleComponent
+  ],
+  imports: [
+    CommonModule,
+    GraficasRoutingModule
+  ]
+})
+export class GraficasModule { }
