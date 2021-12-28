51030b772dcd2dd9ca08497c5f1b9d8a2505a272
lea <leandro.n.aguilera@gmail.com>
2021-12-28 07:53:44 -0300

    Manipular data con RXJS

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -9,12 +10,36 @@ import { GraficasService } from '../../services/graficas.service';
 })
 export class DonaHttpComponent implements OnInit {
 
+  public doughnutChartLabels: string[] = [
+    // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales'
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    // labels: this.doughnutChartLabels,
+    datasets: [
+      // { data: [350, 450, 100] },
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+
   constructor( private graficasService: GraficasService ) { }
 
   ngOnInit(): void {
-    this.graficasService.getUsuariosRedesSociales()
-      .subscribe( data => {
-        console.log(data);
+    // this.graficasService.getUsuariosRedesSociales()
+    //   .subscribe( data => {
+    //     console.log(data);
+    //     const labels = Object.keys(data);
+    //     const values = Object.values( data );
+
+    //     this.doughnutChartLabels = labels;
+    //     this.doughnutChartData.datasets.push(values)
+    //   })
+
+    this.graficasService.getUsuariosRedesSocialesDonaData()
+      .subscribe( ({labels, values}) => {
+        this.doughnutChartLabels = labels;
+        this.doughnutChartData.datasets.push(values)
+
       })
   }
 
